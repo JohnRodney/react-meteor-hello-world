6ea81d8d9c7ea7c503a0dd958cc38d8a85d2c9c2
John Rodney <jrodney@poeticsystems.com>
2015-07-03 13:25:36 -0500

    hello react

---
+++ react-hello-world.jsx
@@ -0,0 +1,11 @@
+var cx = React.addons.classSet;
+
+var helloWorld = ReactMeteor.createClass({
+
+
+  templateName: "helloWorld",
+
+  render: function(){
+    return <div>Hello World</div>;
+  }
+});
